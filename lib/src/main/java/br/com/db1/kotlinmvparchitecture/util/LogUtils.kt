34bb57f15335be19c741f1dda8f98cf5f76f1d67
Vinicius Paes de Camargo <vinicius.camargo@db1.com.br>
2018-05-04 16:16:55 -0300

    Versao inicial do projeto

---
+++ lib/src/main/java/br/com/db1/kotlinmvparchitecture/util/LogUtils.kt
@@ -0,0 +1,20 @@
+package br.com.db1.kotlinmvparchitecture.util
+
+import android.util.Log
+import com.crashlytics.android.Crashlytics
+
+/**
+ * Created by vinicius.camargo on 04/05/2018.
+ */
+object LogUtils {
+    @JvmStatic
+    fun error(tag: String, message: String) {
+        Crashlytics.log(Log.ERROR, tag, message)
+    }
+
+    @JvmStatic
+    fun error(tag: String, throwable: Throwable) {
+        Crashlytics.log(Log.ERROR, tag, Log.getStackTraceString(throwable))
+    }
+
+}
---
+++ lib/src/main/java/br/com/db1/kotlinmvparchitecture/view/IEmptyStateComponent.kt
@@ -0,0 +1,9 @@
+package br.com.db1.kotlinmvparchitecture.view
+
+/**
+ * Created by vinicius.camargo on 04/05/2018.
+ */
+interface IEmptyStateComponent {
+    fun show()
+    fun hide()
+}
